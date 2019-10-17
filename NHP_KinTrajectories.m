82535dc4596bb04c49b9c2b94b04bf9e1e4dd80f
WALIII <bliberti@bu.edu>
2019-10-16 19:18:22 -0700

    update colorspace

--- NHP_KinTrajectories.m
+++ NHP_KinTrajectories.m
@@ -3,7 +3,7 @@ function NHP_KinTrajectories(out,session);
 
 % session = 15;
 % Plot the kin data;
-col = {'r','g','b','y','m','c','w'};
+col = {'r','g','b','y','m','c','g'};
 figure();
 for ii = 1:100; 
 for target = [1:7]; 
@@ -18,6 +18,9 @@ for i = 1:size(out{session}.KinData{target},2)
  [b1 b2] = min(abs(timeVect-9));
  [b1 b3] = min(abs(timeVect-(9.9+diffG)));
  
+  % Get all speeds:
+TarSpeed{target}(:,i) = diffG;
+ 
  if (b2+1+ii)<b3
 plot(toP{i}((b2+ii:b2+1+ii),1),toP{i}((b2+ii:b2+1+ii),2),'Color',col{target})
 plot(toP{i}((b2+1+ii),1),toP{i}((b2+1+ii),2),'*','Color',col{target})
@@ -30,7 +33,7 @@ ylim([1.5 6]);
 end
 end
 title(['time to start ', num2str(-1 + ii*.1)]);
-pause(0.01);
+pause(0.0001);
 clf
 end
 
@@ -39,20 +42,26 @@ end
 
 
 
-% Plot the kin data;
-col = {'r','g','b','y','m','c','w'};
+% Plot the fastest kin data;
+nFast = 10;
+
+col = {'r','g','b','y','m','c','g'};
 figure();
 for target = [1:7]; 
+    clear FastestTimes
+    [ts,FastestTimes] = sort(TarSpeed{target});
 toP = out{session}.KinData{target};
 bound = 50; 
 hold on; 
-for i = 1:size(out{session}.KinData{target},2)
+for ii = 1:nFast%size(out{session}.KinData{target},2)
     % find start
+    i = FastestTimes(ii);
     timeVect = out{session}.KinDatats{target}{i};
     
  diffG = out{session}.starts{target}(i)-(out{session}.ends{target}(i)+10);
- [b1 b2] = min(abs(timeVect-9.9));
- [b1 b3] = min(abs(timeVect-(9.9+diffG)));
+ [b1 b2] = min(abs(timeVect-10));
+ [b1 b3] = min(abs(timeVect-(10+diffG)));
+
 
 plot(toP{i}((b2:b3),1),toP{i}((b2:b3),2),'Color',col{target})
 plot(toP{i}((b3),1),toP{i}((b3),2),'o','Color',col{target})
@@ -67,36 +76,42 @@ end
 
 
 
-% color line:
-figure();
-for target = [5]; 
-toP = out{session}.KinData{target};
-bound = 50; 
-hold on; 
-for i = 1:size(out{session}.KinData{target},2)
-    % find start
-    timeVect = out{session}.KinDatats{target}{i};
-    
- diffG = out{session}.starts{target}(i)-(out{session}.ends{target}(i)+10);
- [b1 b2] = min(abs(timeVect-9.9));
- [b1 b3] = min(abs(timeVect-(9.9+diffG)));
+% 
+% % color line:
+% figure();
+% for target = [5]; 
+% toP = out{session}.KinData{target};
+% bound = 50; 
+% hold on; 
+% for i = 1:size(out{session}.KinData{target},2)
+%     % find start
+%     timeVect = out{session}.KinDatats{target}{i};
+%     
+%  diffG = out{session}.starts{target}(i)-(out{session}.ends{target}(i)+10);
+%  [b1 b2] = min(abs(timeVect-9.9));
+%  [b1 b3] = min(abs(timeVect-(9.9+diffG)));
+% 
+%  if b2>b3; 
+%      disp('out of bounds..');
+%  continue;
+%  end % bad example...
+% p = plot(toP{i}((b2:b3),1),toP{i}((b2:b3),2));
+% %// modified jet-colormap
+% cd = [uint8(jet(length(b2:b3))*255) uint8(ones(length(b2:b3),1))].'; %'
+% 
+% drawnow
+% set(p.Edge, 'ColorBinding','interpolated', 'ColorData',cd);
+% 
+% plot(toP{i}((b3),1),toP{i}((b3),2),'o','Color',col{target})
+% 
+% 
+% xlim([-2 5]);
+% ylim([1.5 6]);
+% end
+% end
+% 
+% 
+% 
+% % Plot fastest trace:
 
- if b2>b3; 
-     disp('out of bounds..');
- continue;
- end % bad example...
-p = plot(toP{i}((b2:b3),1),toP{i}((b2:b3),2));
-%// modified jet-colormap
-cd = [uint8(jet(length(b2:b3))*255) uint8(ones(length(b2:b3),1))].'; %'
-
-drawnow
-set(p.Edge, 'ColorBinding','interpolated', 'ColorData',cd);
-
-plot(toP{i}((b3),1),toP{i}((b3),2),'o','Color',col{target})
-
-
-xlim([-2 5]);
-ylim([1.5 6]);
-end
-end
 
